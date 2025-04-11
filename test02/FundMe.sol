b76159710dc65f00ce88343379c9e97570ef06de
gulu-wang <wangqingping001@126.com>
2025-04-11 20:29:14 +0800

    暂存代码

---
+++ test02/FundMe.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// TODO
+// 1. 创建一个收款函数
+// 2. 记录投资人并查看
+// 3. 在锁定期内，达到目标，生产商可以提款
+// 4. 在锁定期内，没有达到目标，投资人可以在锁定期以后退款cccc
+
+contract FundMe {
+    function fund() external payable {}
+}
