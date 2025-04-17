86d0122e52656f92be5bd53af3afacea38c8002b
gulu-wang <wangqingping001@126.com>
2025-04-17 15:47:19 +0800

    合约demo完成，并部署

--- test02/FundMe_flattened.sol
+++ test02/FundMe_flattened.sol
@@ -1,6 +1,32 @@
 // SPDX-License-Identifier: MIT
+// File: @chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol
+
+
+pragma solidity ^0.8.0;
+
+// solhint-disable-next-line interface-starts-with-i
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
+// File: test02/FundMe.sol
+
+
 pragma solidity ^0.8.20;
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 // TODO
 // 1. 创建一个收款函数
 // 2. 记录投资人并查看
--- test02/FundTokenERC20.sol
+++ test02/FundTokenERC20.sol
@@ -11,14 +11,20 @@ contract FundTokenERC20 is ERC20 {
     }
 
     function mint(uint256 amount) public  {
+        require(fundme.isGetFund(), "the fundme is not complete yet");
         require(fundme.funds(msg.sender)>=amount, "You can't mint more than you have funds");
         _mint(msg.sender, amount);
         fundme.setFundsAmount(msg.sender,  fundme.funds(msg.sender) - amount);
     }
 
-    // 兑换
+    // 将token兑换成指定的东西
     function claim(uint256 amountToClaim) public  {
         // complete cliam
+        require(fundme.isGetFund(), "the fundme is not complete yet");
+        require(balanceOf(msg.sender) > amountToClaim, "you dont have enough token to claim");
+        // TODO
         // burn amountToClaim tokens
+        _burn(msg.sender, amountToClaim);
+
     }
 }
